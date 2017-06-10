f347be61b196f65ddbb065e6ace21e27b029c605
Carlllll <hansmnh93@gmail.com>
2017-06-10 23:56:00 +0900

    Add todo commit

--- src/main/java/kr/ac/cnu/games/poker/Evaluator.java
+++ src/main/java/kr/ac/cnu/games/poker/Evaluator.java
@@ -11,7 +11,12 @@ import java.util.List;
 @Component
 public class Evaluator {
     public List<Hands> evalauteHighHands(List<Hands> handsList) {
-        // do Something
+        // hansList 의 HandType 을 확인
+        // get~~~ 하여 확인한 뒤 ordering
+        // 이 때 같은 HandsType 의 경우 모양 숫자도 확인
+
+        boolean isSameHandsType;
+
         return null;
     }
 
