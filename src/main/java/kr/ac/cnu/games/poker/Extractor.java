0ba1ce42ca535802655fba7c947f0345deebbae3
LeeHeungRok <rhh0208@naver.com>
2017-06-16 00:15:36 +0900

    update extractHighHands for Test Inner Class

--- src/main/java/kr/ac/cnu/games/poker/Extractor.java
+++ src/main/java/kr/ac/cnu/games/poker/Extractor.java
@@ -14,7 +14,10 @@ public class Extractor {
     public Hands extractHighHands(List<Card> cardList) {
         //TODO 주어진 N개의 카드에 대해 가장 높은 조합을 가지는 5개의 카드를 추출해 내야 한다.
         //TODO 한마디로 말해서 카드 중에 제일 높은 족보를 추출
-        return null;
+        Evaluator5zo e5 = new Evaluator5zo();
+        Hands hand = new Hands(e5.evaluate(cardList), cardList);
+
+        return hand;
     }
 
     public Hands extractLowHands(List<Card> cardList) {
