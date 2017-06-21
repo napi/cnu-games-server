e32569ab0ae8b70b7db539588252b535a75482b2
LeeHeungRok <rhh0208@naver.com>
2017-06-21 10:04:59 +0900

    fixed unused method DemoApplicationTest

--- src/test/java/kr/ac/cnu/games/DemoApplicationTests.java
+++ src/test/java/kr/ac/cnu/games/DemoApplicationTests.java
@@ -22,11 +22,6 @@ public class DemoApplicationTests {
 	}
 
 	@Test
-	public void 로얄_스트레이트_플러쉬일_경우(){
-		//TODO 가지고 있는 카드 중 족보 로얄 스트레이트 플러쉬가 있을 경우를 테스트한다
-	}
-
-	@Test
 	public void 가장높은조합추출_플러쉬테스트(){
 		Extractor extractor = new Extractor();
 		List<Card> cardList = new ArrayList<>();
